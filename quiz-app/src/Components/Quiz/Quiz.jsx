7ab25eb108bb5a61ce8f609f0d54bd46d2241f7a
maria <mariyetavishanthi11@gmail.com>
2025-04-18 00:10:41 +0530

    Finished functions

--- quiz-app/src/Components/Quiz/Quiz.jsx
+++ quiz-app/src/Components/Quiz/Quiz.jsx
@@ -1,20 +1,88 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './Quiz.css'
+import { data } from '../../assets/data';
 
 const Quiz = () => {
+    //added questions and answers
+    let [index,setIndex] = useState(0);
+    let[question, setQuestion]  = useState(data[index]);
+    //only one option
+    let [lock, setLock] = useState(false); 
+    let [score,setScore] = useState(0);
+    let [result, setResult] = useState(false);
+
+
+    let option1 = useRef(null);
+    let option2 = useRef(null);
+    let option3 = useRef(null);
+    let option4 = useRef(null);
+
+
+    let option_array = [option1,option2,option3,option4];
+
+    const checkAns = (e,ans)=> { //checkanswers
+        if(lock===false){
+            if (question.ans===ans){
+                e.target.classList.add("correct");
+                setLock(true);
+                setScore(prev=>prev+1);
+            }
+            else{
+                e.target.classList.add("wrong");
+                setLock(true);
+                option_array[question.ans-1].current.classList.add("correct")
+            }
+        }
+        
+    }
+
+    //change question
+    const next = ()=>{
+        if (lock===true){
+
+            if (index===data.length-1){
+                setResult(true);
+                return 0;
+            }
+            setIndex(++index);
+            setQuestion(data[index]);
+            setLock(false);
+            option_array.map((option)=>{
+                option.current.classList.remove("wrong");
+                option.current.classList.remove("correct");
+                return null;
+            })
+        }
+    }
+
+    const reset =() => {
+        setIndex(0);
+        setQuestion(data[0]);
+        setScore(0);
+        setLock(false);
+        setResult(false);
+    }
+    
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr/>
-        <h2>1. What does HTML stand for?</h2>
+        {result?<></>:<>
+        <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li>Hyper Trainer Marking Language</li>
-            <li>Hyper Text Markup Language</li>
-            <li>Hyper Tool Markup Language</li>
-            <li>Hyperlinks Text Managing Language</li>
+            <li ref={option1} onClick={(e)=>{checkAns(e,1)}}>{question.Option1}</li>
+            <li ref={option2} onClick={(e)=>{checkAns(e,2)}}>{question.Option2}</li>
+            <li ref={option3} onClick={(e)=>{checkAns(e,3)}}>{question.Option3}</li>
+            <li ref={option4} onClick={(e)=>{checkAns(e,4)}}>{question.Option4}</li>
         </ul>
-        <button>Next</button>
-        <div className="index">1 of 5 questions</div>
+        <button onClick={next}>Next</button>
+        <div className="index">{index+1} of {data.length} questions</div>
+        </>}
+        {result?<><h2>You Scored {score} out of {data.length}</h2>
+        <button onClick={reset}>Reset</button>
+        </>:<></>}
+        
+        
     </div>
   )
 }
---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+        question: "What does HTML stand for?",
+        Option1: "Hyper Trainer Marking Language",
+        Option2: "Hyper Text Markup Language",
+        Option3: "Hyper Tool Markup Language",
+        Option4: "Hyperlinks Text Managing Language",
+        ans: 2,
+    },
+
+    {
+        question: "What is the correct file extension for a JavaScript file?",
+        Option1: ".java",
+        Option2: ".script",
+        Option3: ".js",
+        Option4: ".jsx",
+        ans: 3,
+    },
+    
+    {
+    question: "Which HTTP status code means 'Not Found'?",
+        Option1: "200",
+        Option2: "301",
+        Option3: "403",
+        Option4: "404",
+        ans: 4,
+    },
+
+    {
+        question: "Which of the following is a JavaScript framework mainly used for building user interfaces?",
+        Option1: "Django",
+        Option2: "Laravel",
+        Option3: "React",
+        Option4: "MongoDB",
+        ans: 3,
+    },
+
+    {
+        question: "Which of the following best describes the difference between frontend and backend development?",
+        Option1: "Frontend runs on the server; backend runs in the browser",
+        Option2: "Backend is only about databases; frontend is only about design",
+        Option3: "Frontend deals with user interface; backend handles logic, database, and server",
+        Option4: "Backend is faster than frontend",
+        ans: 3,
+    },
+
+];
